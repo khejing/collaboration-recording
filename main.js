0e34d17c9f07d065c25357a69071060417972962
khejing <khejing@hotmail.com>
2015-06-06 11:22:45 +0800

    basic screenshot code

---
+++ main.js
@@ -0,0 +1,30 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+require('crash-reporter').start();
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  //TODO: exit when computer restart, to see what node service should do when system down?
+  //app.quit();
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+	  width: 1366,
+	  height: 768,
+	  show: false,
+	  frame: false,
+	  'node-integration': false,
+	  preload: __dirname + '/preload.js'
+  });
+
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  //mainWindow.openDevTools();
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
---
+++ preload.js
@@ -0,0 +1,2 @@
+// Load remote module here and remove node integration
+window.__electron__remote = require('remote');

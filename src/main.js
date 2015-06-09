aef0fc552eaf9a4818490eb4e02cab386006d7a4
khejing <khejing@hotmail.com>
2015-06-09 20:08:13 +0800

    test for jason

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ var app = require('app');
 var BrowserWindow = require('browser-window');
 var ipc = require('ipc');
 var fs = require('fs');
+var jason = require('JASON');
 
 require('crash-reporter').start();
 
@@ -31,12 +32,18 @@ app.on('ready', function() {
   });
 });
 
-ipc.on('WriteFile', function(event, filename, buf){
-  console.log("got message to write file: "+filename+", time is "+Date.now());
+ipc.on('WriteFile', function(event, filename){
+  console.log("going to capture in main: "+Date.now());
+  capturePage(function(buf) {
+    console.log("capture finished in main: "+Date.now());
+    var mybuffer = jason.stringify(buf);
+	console.log("serialization finished in main: "+Date.now());
+  }
+  /*console.log("got message to write file: "+filename+", time is "Date.now());
   fs.writeFile(filename, buf, function(err) {
     console.log("write file finished: "+Date.now());
     if(err){
       console.log(err);
     }
-  });
+  });*/
 });
